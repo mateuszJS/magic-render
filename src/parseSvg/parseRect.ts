fdcc836196f207df466f5a2c22b2b1bca502e873
mateuszJS <mate.walendzik@gmail.com>
2025-08-03 17:53:32 +0200

    support bezier curves from svg files

---
+++ src/parseSvg/parseRect.ts
@@ -0,0 +1,27 @@
+import { STRAIGHT_LINE_HANDLE } from './const'
+import { Line } from './types'
+
+export default function parseRect(width: number, height: number, svgHeight: number): Line[] {
+  // Define the four corners of the rectangle in Cartesian space
+  const topLeft: Point = { x: 0, y: height }
+  const topRight: Point = { x: width, y: height }
+  const bottomRight: Point = { x: width, y: 0 }
+  const bottomLeft: Point = { x: 0, y: 0 }
+
+  // Create four lines that form the rectangle
+  const lines: Line[] = [
+    // Top edge: left to right
+    [topLeft, STRAIGHT_LINE_HANDLE, STRAIGHT_LINE_HANDLE, topRight],
+
+    // Right edge: top to bottom
+    [topRight, STRAIGHT_LINE_HANDLE, STRAIGHT_LINE_HANDLE, bottomRight],
+
+    // Bottom edge: right to left
+    [bottomRight, STRAIGHT_LINE_HANDLE, STRAIGHT_LINE_HANDLE, bottomLeft],
+
+    // Left edge: bottom to top
+    [bottomLeft, STRAIGHT_LINE_HANDLE, STRAIGHT_LINE_HANDLE, topLeft],
+  ]
+
+  return lines
+}
