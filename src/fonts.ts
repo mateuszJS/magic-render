4f9a041a2b7d3d6b2ab019b5a982be9ed48e5327
mateuszJS <mate.walendzik@gmail.com>
2025-11-22 11:00:08 +0100

    load woff2

--- src/fonts.ts
+++ src/fonts.ts
@@ -1,39 +1,28 @@
 import fontFile from '../icons/Outfit.woff2'
-// import fontFile from '../icons/GoogleSans-Regular.ttf'
 import opentype from 'opentype.js'
-import * as fontkit from 'fontkit'
-import type { Font } from 'opentype.js'
 import parsePathData from 'svgToShapes/parsePathData'
 import * as Textures from 'textures'
 import * as Logic from 'logic/index.zig'
 import { Point } from 'types'
+import decompressWoff2 from 'utils/decompressWoff2.mjs'
 
 const DEFAULT_SPACE = 250 // expressed in font units
 const ENTER = 10
 
-let font: Font
-let fontkitFont: fontkit.Font | fontkit.FontCollection
-const x = 10
+let font: opentype.Font
 
 export async function loadFont() {
   try {
-    // <input type=file accept=".woff2" onchange="files[0].arrayBuffer().then(buf=>form.size.innerText=Module.decompress(buf).length)">
-    // if (1 === 2 + x) {
     const res = await fetch(fontFile)
     const buffer = await res.arrayBuffer()
-    fontkitFont = fontkit.create(new Uint8Array(buffer) as Buffer<ArrayBufferLike>)
-    console.log(fontkitFont)
-    console.log((fontkitFont as unknown as { _decompress: () => void })._decompress())
-    const b = (fontkitFont as unknown as { stream: { buffer: Uint8Array } }).stream.buffer.buffer
-    console.log(b)
-    font = opentype.parse(b)
-    // }
-    // const response = await fetch(fontFile)
-    // const arrayBuffer = await response.arrayBuffer()
-    // const buf = new Uint8Array(arrayBuffer) as Buffer<ArrayBufferLike>
-    // fontkitFont = fontkit.create(buf)
-    // console.log(fontkitFont)
-    // fontkitFont = (await fontkit.open(fontFile)) as fontkit.Font
+    const deocmpreassed = decompressWoff2.decompress(buffer)
+
+    // Create a copy of the buffer because deocmpreassed is a view into WASM memory
+    // and .buffer would return the whole WASM heap
+    const fontBuffer = deocmpreassed.slice(0).buffer
+
+    console.log('deocmpreassed', deocmpreassed)
+    font = opentype.parse(fontBuffer)
   } catch (err) {
     console.error('Failed to load font', err)
   }
