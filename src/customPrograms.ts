ec69c09dc331bd19217609a3f197c73a855a0713
mateuszJS <mate.walendzik@gmail.com>
2025-11-26 18:29:46 +0100

    improve ux of coding

--- src/customPrograms.ts
+++ src/customPrograms.ts
@@ -1,20 +1,32 @@
 import getDrawShape from 'WebGPU/programs/drawShape/getProgram'
 import customProgramWrapper from 'WebGPU/programs/drawShape/custom-program-wrapper.wgsl'
 import { device, presentationFormat } from 'WebGPU/device'
+import { CustomProgramError } from 'types'
 
 interface CustomProgram {
   code: string
   callback: ReturnType<typeof getDrawShape>
+  errors: CustomProgramError[]
 }
 
-const customPrograms = new Map<number, CustomProgram>()
+/* we assume that one code is assosiated with an id, if code changes,
+id has to be updated as well. Old, unused id should be cleaned up at
+some point in the future(e.g. during setSnapshot) */
+let customPrograms: Map<number, CustomProgram>
+let altProgramOnErr: ReturnType<typeof getDrawShape> | null
+let programIdCounter: number
 
-let compilationErrorCallback: (info: GPUCompilationInfo) => void = () => {}
-export function setCallbackCompilationError(callback: (info: GPUCompilationInfo) => void): void {
-  compilationErrorCallback = callback
+export function init(): void {
+  customPrograms = new Map<number, CustomProgram>()
+  altProgramOnErr = null
+  programIdCounter = 0
 }
 
-let altProgramOnErr: ReturnType<typeof getDrawShape> | null = null
+let triggerSnapshotsUpdate: (programId: number) => void = () => {}
+export function setTriggerSnapshotsUpdateCallback(callback: typeof triggerSnapshotsUpdate): void {
+  triggerSnapshotsUpdate = callback
+}
+
 function getAlternativeProgramOnError() {
   if (!altProgramOnErr) {
     altProgramOnErr = getDrawShape(
@@ -24,44 +36,61 @@ function getAlternativeProgramOnError() {
         '${CUSTOM_PROGRAM_CODE}',
         'let s=20.0;let p=floor(uv*s);let c=(p.x+p.y)%2.0;color=vec4f(c,c,c,1);'
       ),
-      4 * 4
+      4 * 4,
+      (info) => {
+        console.warn('Alternative program compilation info:', info)
+      }
     )
   }
   return altProgramOnErr
 }
 
-function createProgram(code: string): CustomProgram {
-  const result = {
+function createProgram(
+  code: string,
+  newId: number,
+  placeholderProgramCb: ReturnType<typeof getDrawShape>
+): CustomProgram {
+  const program: CustomProgram = {
     code,
-    callback: (() => {}) as ReturnType<typeof getDrawShape>,
+    callback: placeholderProgramCb,
+    errors: [],
   }
 
-  result.callback = getDrawShape(
+  const compiledProgramCb = getDrawShape(
     device,
     presentationFormat,
     customProgramWrapper.replace('${CUSTOM_PROGRAM_CODE}', code),
     4 * 4,
     (info) => {
-      if (info.messages.some((msg) => msg.type === 'error')) {
-        result.callback = getAlternativeProgramOnError()
-        compilationErrorCallback(info)
+      const errors = info.messages.filter((msg) => msg.type === 'error')
+      if (errors.length > 0) {
+        program.errors = errors
+      } else {
+        program.callback = compiledProgramCb
+        triggerSnapshotsUpdate(newId)
       }
     }
   )
-  return result
+  return program
 }
 
-let programIdCounter = 0
 export function getCustomProgramId(id: number | undefined, code: string): number {
+  let placeholderProgramCb = getAlternativeProgramOnError()
+
   if (typeof id === 'number') {
     const program = customPrograms.get(id)
-    if (program && program.code === code) {
-      return id
+    if (program) {
+      if (program.code === code) {
+        return id // cached program is still valid
+      } else {
+        placeholderProgramCb = program.callback
+        customPrograms.delete(id) // outdated cache
+      }
     }
   }
 
   const newId = programIdCounter++
-  customPrograms.set(newId, createProgram(code))
+  customPrograms.set(newId, createProgram(code, newId, placeholderProgramCb))
   return newId
 }
 
